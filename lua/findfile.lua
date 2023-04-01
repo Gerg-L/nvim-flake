8f9e2a813119423dbef31f652e89caa975bec75d
Gerg-L <GregLeyda@proton.me>
2023-04-01 16:31:36 -0400

    updated nvim-tree config

--- lua/findfile.lua
+++ lua/findfile.lua
@@ -1,5 +1,23 @@
 -- SECTION: nvim-tree
+local function open_nvim_tree(data)
 
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 require'nvim-tree'.setup({
   disable_netrw = true,
   hijack_netrw = true,
@@ -47,10 +65,6 @@ require'nvim-tree'.setup({
   },
   filters = {
     dotfiles = false,
-    custom = {
-      ".git",
-      ".cache",
-    },
   },
 })
 
