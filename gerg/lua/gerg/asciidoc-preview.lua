d7ac1edd212ce15621dda3143b8f60d0092883af
jirafey <jkas@ask.com>
2024-07-29 14:12:18 +0200

    A

---
+++ gerg/lua/gerg/asciidoc-preview.lua
@@ -0,0 +1,21 @@
+require("ascii-preview").setup({
+  server = {
+    -- Determines how the AsciiDoc file is converted to HTML for the preview.
+    -- `js`  - asciidoctor.js (no local installation needed)
+    -- `cmd` - asciidoctor command (local installation needed)
+    converter = 'js',
+
+    -- Determines the local port of the preview website.
+    -- Must be between 10000 and 65535.
+    port = 11235,
+  },
+  preview = {
+    -- Determines the scroll position of the preview website.
+    -- `current` - Keep current scroll position
+    -- `start`   - Start of the website
+    -- `sync`    - (experimental) Same (similar) position as in Neovim
+    --             => inaccurate, because very content dependent
+    position = 'current',
+  },
+})
+vim.keymap.set('n', '<Leader>cp', ':AsciiDocPreview<CR>', { desc = 'Preview AsciiDoc document' })
