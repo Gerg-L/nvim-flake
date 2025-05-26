f21def6b0cdaa84a43fd6981f5c97d4713665bf6
Gerg-L <GregLeyda@proton.me>
2025-05-26 13:15:48 -0400

    feat(noice): filter deprecation messages

--- lua/lazy/noice.lua
+++ lua/lazy/noice.lua
@@ -20,6 +20,15 @@ return {
     end,
     after = function()
       require("noice").setup({
+        routes = {
+          {
+            filter = {
+              event = "msg_show",
+              find = 'is deprecated. Run ":checkhealth vim.deprecated" for more information',
+            },
+            opts = { skip = true },
+          },
+        },
         lsp = {
           override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
@@ -28,11 +37,11 @@ return {
         },
 
         presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
+          bottom_search = true,    -- use a classic bottom cmdline for search
+          command_palette = true,  -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = true, -- add a border to hover docs and signature help
+          inc_rename = false,      -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true,   -- add a border to hover docs and signature help
         },
 
         format = {
