64b462dff6cb14f2ed69ba7768dd28f9870c65a6
Gerg-L <GregLeyda@proton.me>
2025-05-26 13:15:19 -0400

    fix: fzf-lua

--- lua/lazy/fzf.lua
+++ lua/lazy/fzf.lua
@@ -1,18 +1,54 @@
 return {
   "fzf-lua",
   cmd = "FzfLua",
-  event = "DeferredUIEnter",
   after = function()
     vim.env.FZF_DEFAULT_OPTS = "--layout=reverse --inline-info"
-    require("fzf-lua").setup({ "telescope" })
+    require("fzf-lua").setup({ "telescope", "hide" })
+    FzfLua.register_ui_select()
   end,
   keys = {
-    { "<leader>ff", "<cmd> FzfLua files<CR>", desc = "fzf files" },
-    { "<leader>fg", "<cmd> FzfLua live_grep_native<CR>", desc = "fzf ripgrep (native)" },
-    { "<leader>fG", "<cmd> FzfLua live_grep<CR>", desc = "fzf ripgrep" },
-    { "<leader>fr", "<cmd> FzfLua resume<CR>", desc = "fzf resume" },
-    { "<leader>fb", "<cmd> FzfLua buffers<CR>", desc = "fzf buffers" },
-    { "<leader>la", "<cmd> FzfLua lsp_code_actions<CR>", desc = "fzf code actions" },
+    {
+      "<leader>ff",
+      function()
+        FzfLua.files()
+      end,
+      desc = "fzf files",
+    },
+    {
+      "<leader>fg",
+      function()
+        FzfLua.live_grep_native()
+      end,
+      desc = "fzf ripgrep (native)",
+    },
+    {
+      "<leader>fG",
+      function()
+        FzfLua.live_grep()
+      end,
+      desc = "fzf ripgrep",
+    },
+    {
+      "<leader>fr",
+      function()
+        FzfLua.resume()
+      end,
+      desc = "fzf resume",
+    },
+    {
+      "<leader>fb",
+      function()
+        FzfLua.buffers()
+      end,
+      desc = "fzf buffers",
+    },
+    {
+      "<leader>la",
+      function()
+        FzfLua.lsp_code_actions()
+      end,
+      desc = "fzf code actions",
+    },
   },
 
   wk = {
