b11ecb61fe305e99083fb6e2ed7a0cbbcbf29a5e
Gerg-L <GregLeyda@proton.me>
2025-05-28 21:07:52 -0400

    fix: disable undercurls

--- lua/gerg/misc.lua
+++ lua/gerg/misc.lua
@@ -57,7 +57,7 @@ vim.g.moonflyCursorColor = true
 vim.g.moonflyNormalFloat = true
 vim.g.moonflyTerminalColors = true
 vim.g.moonflyTransparent = true
-vim.g.moonflyUndercurls = true
+vim.g.moonflyUndercurls = false
 vim.g.moonflyUnderlineMatchParen = true
 vim.g.moonflyVirtualTextColor = true
 vim.cmd.colorscheme("moonfly")
