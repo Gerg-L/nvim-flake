c22a81ed73c293049b75fd3f1cec385e306eb290
Gerg-L <GregLeyda@proton.me>
2026-02-08 13:55:47 -0500

    feat: re-add tresitter opts

--- lua/gerg/treesitter.lua
+++ lua/gerg/treesitter.lua
@@ -1,8 +1,19 @@
 vim.g._ts_force_sync_parsing = true
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = vim.treesitter.language._complete(),
-  group = vim.api.nvim_create_augroup("LoadTreesitter", {}),
-  callback = function()
-    vim.treesitter.start()
-  end,
+	pattern = vim.treesitter.language._complete(),
+	group = vim.api.nvim_create_augroup("LoadTreesitter", {}),
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
+require("nvim-treesitter").setup({
+	modules = {},
+	sync_install = false,
+	ignore_install = {},
+	ensure_installed = {},
+	auto_install = false,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
 })
